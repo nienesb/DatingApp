34fe2841394f612a5c41858aa85062a367c5e648
DESKTOP-HMQEJBB\btaluy <berfu.taluy@navara.nl>
2017-05-18 21:06:49 +0200

    Made some progress with data converting to a listview.

--- app/src/main/java/com/teamdating/datingapp/Activities/DagOmzet.java
+++ app/src/main/java/com/teamdating/datingapp/Activities/DagOmzet.java
@@ -4,11 +4,13 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.studioidan.httpagent.HttpAgent;
 import com.studioidan.httpagent.JsonArrayCallback;
 import com.studioidan.httpagent.JsonCallback;
+import com.teamdating.datingapp.JSONAdapter;
 import com.teamdating.datingapp.R;
 import com.teamdating.datingapp.StorageProvider;
 
@@ -16,15 +18,20 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DagOmzet extends AppCompatActivity {
 
     JSONArray platforms = new JSONArray();
-
+    ArrayList stats = new ArrayList();
+    private ListView mListView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dag_omzet);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        mListView = (ListView) findViewById(R.id.list_view);
         setSupportActionBar(toolbar);
         getPlatforms();
 
@@ -62,14 +69,32 @@ public class DagOmzet extends AppCompatActivity {
                                     try {
                                         if (platforms.getJSONObject(i).getInt("id") == jsonArray.getJSONObject(p).getInt("platformId")) {
                                             jsonArray.getJSONObject(p).put("platform",platforms.getJSONObject(i));
-                                            Toast.makeText(DagOmzet.this, "HET IS GELUKT!", Toast.LENGTH_SHORT).show();
+                                            //Toast.makeText(DagOmzet.this, "HET IS GELUKT!", Toast.LENGTH_SHORT).show();
                                         }
                                     } catch (JSONException e) {
                                         e.printStackTrace();
                                     }
                                 }
-
                             }
+                            for(int p = 0; p < jsonArray.length(); p++) {
+                                try {
+                                    stats.add(jsonArray.getJSONObject(p));
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+//                            Object object = stats.get(0);
+                                try {
+                                    Toast.makeText(DagOmzet.this, jsonArray.getJSONObject(0).get("startDate").toString(), Toast.LENGTH_LONG).show();
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+
+//                            String[] from = new String[] {"id"};
+//                            int[] to = new int[] { R.id.list_view };
+//                            JSONAdapter jsonAdapter = new JSONAdapter(DagOmzet.this, jsonArray, R.layout.content_dag_omzet, from, to );
+//                            mListView.setAdapter(jsonAdapter);
+
                         } else {
                             Toast.makeText(DagOmzet.this, "Geen resultaten van daily-targets ontvangen", Toast.LENGTH_SHORT).show();
                         }
