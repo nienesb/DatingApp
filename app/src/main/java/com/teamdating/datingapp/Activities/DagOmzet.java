eeec39c4da59a2c5451bef69d42d19aded293893
Milan Steenwinkel <milan.steenwinkel@navara.nl>
2017-05-22 19:21:47 +0200

    Some refactor

--- app/src/main/java/com/teamdating/datingapp/Activities/DagOmzet.java
+++ app/src/main/java/com/teamdating/datingapp/Activities/DagOmzet.java
@@ -3,23 +3,18 @@ package com.teamdating.datingapp.Activities;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.text.TextUtils;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import com.studioidan.httpagent.HttpAgent;
 import com.studioidan.httpagent.JsonArrayCallback;
-import com.studioidan.httpagent.JsonCallback;
-import com.teamdating.datingapp.JSONAdapter;
 import com.teamdating.datingapp.R;
 import com.teamdating.datingapp.StorageProvider;
 
 import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class DagOmzet extends AppCompatActivity {
 
@@ -64,42 +59,47 @@ public class DagOmzet extends AppCompatActivity {
                     @Override
                     protected void onDone(boolean success, JSONArray jsonArray) {
                         if (jsonArray != null) {
-                            for (int i = 0; i < platforms.length(); i++) {
-                                for(int p = 0; p < jsonArray.length(); p++) {
-                                    try {
-                                        if (platforms.getJSONObject(i).getInt("id") == jsonArray.getJSONObject(p).getInt("platformId")) {
-                                            jsonArray.getJSONObject(p).put("platform",platforms.getJSONObject(i));
-                                            //Toast.makeText(DagOmzet.this, "HET IS GELUKT!", Toast.LENGTH_SHORT).show();
-                                        }
-                                    } catch (JSONException e) {
-                                        e.printStackTrace();
-                                    }
-                                }
-                            }
-                            for(int p = 0; p < jsonArray.length(); p++) {
-                                try {
-                                    stats.add(jsonArray.getJSONObject(p));
-                                } catch (JSONException e) {
-                                    e.printStackTrace();
-                                }
-                            }
-//                            Object object = stats.get(0);
-                                try {
-                                    Toast.makeText(DagOmzet.this, jsonArray.getJSONObject(0).get("startDate").toString(), Toast.LENGTH_LONG).show();
-                                } catch (JSONException e) {
-                                    e.printStackTrace();
-                                }
+                            jsonArray = addPlatformsToStats(jsonArray);
+                            jsonToList(jsonArray);
+//                            try {
+//                                    Toast.makeText(DagOmzet.this, jsonArray.getJSONObject(0).get("startDate").toString(), Toast.LENGTH_LONG).show();
+//                                } catch (JSONException e) {
+//                                    e.printStackTrace();
+//                                }
 
-//                            String[] from = new String[] {"id"};
-//                            int[] to = new int[] { R.id.list_view };
-//                            JSONAdapter jsonAdapter = new JSONAdapter(DagOmzet.this, jsonArray, R.layout.content_dag_omzet, from, to );
-//                            mListView.setAdapter(jsonAdapter);
+
 
                         } else {
                             Toast.makeText(DagOmzet.this, "Geen resultaten van daily-targets ontvangen", Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
+    }
+
+    private JSONArray addPlatformsToStats(JSONArray jsonArray) {
+        for (int i = 0; i < platforms.length(); i++) {
+            for(int p = 0; p < jsonArray.length(); p++) {
+                try {
+                    if (platforms.getJSONObject(i).getInt("id") == jsonArray.getJSONObject(p).getInt("platformId")) {
+                        jsonArray.getJSONObject(p).put("platform",platforms.getJSONObject(i));
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return jsonArray;
+    }
+
+    private void jsonToList(JSONArray jsonArray) {
+        for(int p = 0; p < jsonArray.length(); p++) {
+            try {
+                stats.add(jsonArray.getJSONObject(p));
+                Toast.makeText(DagOmzet.this, stats.get(0).toString(), Toast.LENGTH_LONG).show();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     private void showDagomzet(JSONArray jsonArray) {
